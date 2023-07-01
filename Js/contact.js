01aa065e965f98b301c1198e78e5fd12cced8c51
Ahmad Syahran Zidane <zidanenoor17@gmail.com>
2023-07-01 10:46:02 +0700

    revision task day 2

---
+++ Js/contact.js
@@ -0,0 +1,36 @@
+// Get Element
+
+function submitData() {
+  let name = document.getElementById("input-name").value;
+  let email = document.getElementById("input-email").value;
+  let phone = document.getElementById("input-phone").value;
+  let subject = document.getElementById("input-subject").value;
+  let message = document.getElementById("input-message").value;
+
+  // read the data
+//   console.log(name);
+//   console.log(email);
+//   console.log(phone);
+//   console.log(select);
+//   console.log(message);
+
+  // a condition if the value is null
+  if (name == "") {
+    return alert("Masukkan value terlebih dahulu");
+  } else if (email == "") {
+    return alert("Masukkan value terlebih dahulu");
+  } else if (phone == "") {
+    return alert("Masukkan value terlebih dahulu");
+  } else if (subject == "") {
+    return alert("Masukkan value terlebih dahulu");
+  } else if (message == "") {
+    return alert("Masukkan value terlebih dahulu");
+  }
+
+    //mailto
+    let a = document.createElement("a");
+    a.href = `mailto:${email}?subject=${subject}&body=halo, Hello my name is ${name}, 
+    
+    ${message}`;
+    a.click()
+}
